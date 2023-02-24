84ddb014ec5940d5a19d5b71f7ffee7f48e8a337
WinstonSmith13 <krouk.medy@gmail.com>
2023-02-24 20:02:53 +0100

    build: mvc working with dasboard display.

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -1,6 +1,9 @@
-<?php 
+<?php
 
 
+use WINSTON\movieAppNuxt\Controllers\Dashboards;
+
+$dashboard = new Dashboards();
+echo $dashboard->display();
 
 
-?>
--- App/Controllers/Dashboards.php
+++ App/Controllers/Dashboards.php
@@ -1,3 +1,9 @@
 <?php
 
 namespace WINSTON\movieAppNuxt\Controllers;
+
+class Dashboards extends Controller{
+    public function display(): array|string {
+        return $this->render('layouts.default', 'templates.dashboard');
+    }
+}
