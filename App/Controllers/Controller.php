ef8ca30cd3c761a7e094b2fce72d7a0a14f7274b
WinstonSmith13 <krouk.medy@gmail.com>
2023-02-24 16:08:23 +0100

    build: working on the mvc structure and the Dashboard controller.

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -10,5 +10,16 @@ class Controller{
     public function render($layout, $view, $data=null): array|string{
         $layout_ar = explode('.', $layout);
         ob_start();
+        require_once (self::VIEWPATH . ucfirst($layout_ar[0]) . '/' . $layout_ar[1] . self::EXT);
+        $layout_content = ob_get_contents();
+        ob_end_clean();
+        $layout = str_replace('{pageTitle}', self::PAGETITLE, $layout_content);
+
+        $view_ar = explode('.', $view);
+        ob_start();
+        require_once (self::VIEWPATH . ucfirst($view_ar[0]) . '/' . $view_ar[1] . self::EXT);
+        $view_content = ob_get_contents();
+        ob_end_clean();
+        return  str_replace('{pageContent}', $view_content, $layout);
     }
 }
---
+++ Views/Layouts/default.tmpl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>{pageTitle}</title>
+</head>
+<body>
+<?php
+require_once(__DIR__ . '/../../Views/Partials/header.tmpl.php');
+?>
+<main>
+    {pageContent}
+</main>
+
+<script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
